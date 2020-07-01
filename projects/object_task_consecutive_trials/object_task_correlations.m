cbe8bb6780fd35411aa7824ac8ba1dbafaa5d0d3
Marc Normandin <marc.normandin@utsa.edu>
2020-07-01 16:14:17 -0500

    Added code to support a single hab session and no test.

--- projects/object_task_consecutive_trials/object_task_correlations.m
+++ projects/object_task_consecutive_trials/object_task_correlations.m
@@ -1,9 +1,12 @@
 function object_task_correlations(pipe)
 % There are two forms of this task:
-% 1) hab session containing 4 trials, and test session containing 1 trial
-% 2) 5 separate session each containing 1 trial.
+% 1) hab session containing 4 trials, and NO test session
+% 2) hab session containing 4 trials, and test session containing 1 trial
+% 3) 5 separate session each containing 1 trial.
 
-    if pipe.Experiment.getNumSessions() == 2
+    if pipe.Experiment.getNumSessions() == 1
+        object_task_correlations_for_4hab_0test(pipe);
+    elseif pipe.Experiment.getNumSessions() == 2
         object_task_correlations_for_4hab_1test(pipe);
     elseif pipe.Experiment.getNumSessions() == 5
         object_task_correlations_for_5sessions(pipe);
