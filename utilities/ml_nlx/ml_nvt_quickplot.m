286157c0f07113bb7eef752332c738ace633bc83
marcnormandin <normandin.utb@gmail.com>
2020-08-18 13:03:46 -0500

    Modified since last used.

--- utilities/ml_nlx/ml_nvt_quickplot.m
+++ utilities/ml_nlx/ml_nvt_quickplot.m
@@ -10,14 +10,47 @@ CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S = 10;
 numTrials = ml_nlx_nvt_get_num_trials(nvtFullFilename, CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S);
 fprintf('num trials = %d\n', numTrials);
 
+ %function asdfadf()
+
+close all
+clear all
+clc
+
+nvtFullFilename = fullfile(pwd, 'VT1.nvt');
+CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S = 10;
+
+numTrials = ml_nlx_nvt_get_num_trials(nvtFullFilename, CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S);
+fprintf('num trials = %d\n', numTrials);
+
+ 
+
+ %function asdfadf()
+
+close all
+clear all
+clc
+
+nvtFullFilename = fullfile(pwd, 'VT1.nvt');
+CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S = 10;
+
+numTrials = ml_nlx_nvt_get_num_trials(nvtFullFilename, CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S);
+fprintf('num trials = %d\n', numTrials);
+
  
 figure
-p = 3; q = 4; k = 1;
+p = 4; q = 4; k = 1;
 for iTrial = 1:numTrials
     ax(k) = subplot(p,q,k);
     k = k + 1;
         [t_ms, x_px, y_px, theta_deg] =  ml_nlx_nvt_get_raw_trial(iTrial, nvtFullFilename, CONFIG_NVT_FILE_TRIAL_SEPARATION_THRESHOLD_S);
 
+    badxi = find(x_px == 0);
+    badyi = find(y_px == 0);
+    badi = intersect(badxi, badyi);
+    x_px(badi) = [];
+    y_px(badi) = [];
+    t_ms(badi) = [];
+    theta_deg(badi) = [];
     %subplot(p,q,k);
     h1 = plot(x_px, y_px, 'm.');
     %[xx,yy,indx,sect] = graphpoints();
